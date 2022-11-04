dfafa73bcac58cd5cc3c93b7bd701f02e38340ba
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-11-04 18:57:52 -0300

    Creates some constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greenLightOne = Color(0xff63b5af);
+  static const Color greenLightTwo = Color(0xff438883);
+  static const Color white = Color(0xffffffff);
+  static const Color iceWhite = Color(0xffeef8f7);
+  static const Color grey = Color(0xff444444);
+
+}
